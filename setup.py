d9a1d6ac2cc2ccdd4abc9cf42e9d90c63b3dbb94
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-03 11:44:22 +0100

    add python3.8 support

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
         'django>=2.2',
