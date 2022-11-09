2ace7530cff36bc57cd4a57d249c72fc87faabdb
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-09 10:16:34 +0100

    fix default autofield

--- mapbox_baselayer/apps.py
+++ mapbox_baselayer/apps.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class MapboxBaselayerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "django.db.models.AutoField"
     name = 'mapbox_baselayer'
     verbose_name = _('MapBox Utils')
