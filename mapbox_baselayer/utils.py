a1f9f55d26a0cbb67aa759ae786947a3d0471cdb
J-E Castagnede <j.e.castagnede@gmail.com>
2026-02-13 15:52:51 +0100

    feat: refactor map base layer handling by moving constants and logic to utils

---
+++ mapbox_baselayer/utils.py
@@ -0,0 +1,53 @@
+from django.urls import reverse
+
+from mapbox_baselayer.models import MapBaseLayer
+
+DEFAULT_OSM_TILEJSON = {
+    "version": 8,
+    "sources": {
+        "osm": {
+            "type": "raster",
+            "tiles": [
+                "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            ],
+            "minzoom": 0,
+            "maxzoom": 19,
+            "tileSize": 256,
+            "attribution": '<a href="https://www.openstreetmap.org/copyright">OSM Contributors</a>',
+        }
+    },
+    "layers": [
+        {
+            "id": "osm-background",
+            "type": "raster",
+            "source": "osm",
+        }
+    ],
+    "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+}
+
+
+def get_map_base_layers():
+    layers = MapBaseLayer.objects.filter(enabled=True)
+    base_layers = layers.filter(is_overlay=False)
+    overlay_layers = layers.filter(is_overlay=True)
+
+    data = {
+        "base_layers": [
+            {"name": bl.name, "slug": bl.slug, "url": bl.url} for bl in base_layers
+        ]
+        if base_layers.filter(enabled=True).exists()
+        else [
+            {
+                "name": "OSM",
+                "slug": "osm",
+                "url": reverse("mapbox_baselayer:default-osm-tilejson"),
+            }
+        ],
+        "overlay_layers": [
+            {"name": ol.name, "slug": ol.slug, "url": ol.url} for ol in overlay_layers
+        ],
+    }
+    return data
