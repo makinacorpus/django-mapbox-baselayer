43dc67a5584a54352b84acff5611c0a0f953ca6a
J-E Castagnede <j.e.castagnede@gmail.com>
2026-07-07 17:31:54 +0200

    fix: update test to reflect new maxBounds nested array format

--- mapbox_baselayer/tests/test_views.py
+++ mapbox_baselayer/tests/test_views.py
@@ -207,4 +207,4 @@ class ViewsCoverageTestCase(TestCase):
         self.assertEqual(item["options"]["maxZoom"], 10)
         self.assertAlmostEqual(item["options"]["center"][0], 2.0)
         self.assertAlmostEqual(item["options"]["center"][1], 3.0)
-        self.assertEqual(item["options"]["maxBounds"], [1.0, 2.0, 3.0, 4.0])
+        self.assertEqual(item["options"]["maxBounds"], [[1.0, 2.0], [3.0, 4.0]])
