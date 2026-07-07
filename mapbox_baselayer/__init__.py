66a902a5aed7ce3dce29a0747c95dec1d28c999f
J-E Castagnede <j.e.castagnede@gmail.com>
2026-07-07 17:27:11 +0200

    feat: update pmtiles maxBounds format and include __version__ from VERSION.md

--- mapbox_baselayer/__init__.py
+++ mapbox_baselayer/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 default_app_config = "mapbox_baselayer.apps.MapboxBaselayerConfig"
-with open("VERSION.md") as f:
+with open(os.path.join(os.path.dirname(__file__), "VERSION.md")) as f:
     __version__ = f.read().strip()
